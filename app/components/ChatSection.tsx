302f6a1fc32e14ccc727d65a2ddb9d48a9668e43
E-Joon Ko <e.joon.ko@umain.com>
2024-11-16 11:10:58 +0100

    chat working

--- app/components/ChatSection.tsx
+++ app/components/ChatSection.tsx
@@ -8,6 +8,7 @@ import {
   fetchExchange,
   gql,
   subscriptionExchange,
+  useMutation,
 } from 'urql';
 import { useState } from 'react';
 import { Message } from '../types/chatTypes';
@@ -18,9 +19,21 @@ interface ChatSectionProps {
   visitId: string;
 }
 
-const messagesQuery = gql`
+const GET_MESSAGES = gql`
   subscription Chat_messages($visitId: uuid!) {
     chat_messages(where: { visit_id: { _eq: $visitId } }) {
+      from_user_id
+      id
+      message
+      visit_id
+    }
+  }
+`;
+
+const SEND_MESSAGE = gql`
+  mutation Insert_chat_messages_one($object: chat_messages_insert_input!) {
+    insert_chat_messages_one(object: $object) {
+      created_at
       from_user_id
       id
       message
@@ -57,9 +70,21 @@ export default function ChatSection({ visitId, userId }: ChatSectionProps) {
     return <></>;
   }
 
-  const [data, setData] = useState<Message[] | undefined>([]);
+  const [_, sendMessageMutation] = useMutation(SEND_MESSAGE);
 
-  client.subscription(messagesQuery, { visitId: visitId }).subscribe((result) => {
+  const [data, setData] = useState<Message[] | undefined>([]);
+  const [message, setMessage] = useState<string | undefined>();
+
+  const sendMessage = (message: string | undefined) => {
+    if (message) {
+      sendMessageMutation({
+        object: { from_user_id: userId, visit_id: visitId, message: message },
+      });
+      setMessage(undefined);
+    }
+  };
+
+  client.subscription(GET_MESSAGES, { visitId: visitId }).subscribe((result) => {
     result.data?.chat_messages && setData(result.data?.chat_messages);
   });
 
@@ -72,8 +97,15 @@ export default function ChatSection({ visitId, userId }: ChatSectionProps) {
       </Text>
       <ChatMessageSection messages={data} userId={userId} />
       <Flex gap={15} p={15} bg={'cyan'}>
-        <TextInput placeholder="Send a message here!" style={{ width: '95%' }} />
-        <Button>{'Send'}</Button>
+        <TextInput
+          value={message}
+          onChange={(event) => setMessage(event.currentTarget.value)}
+          placeholder="Send a message here!"
+          style={{ width: '95%' }}
+        />
+        <Button disabled={!message} onClick={() => sendMessage(message)}>
+          {'Send'}
+        </Button>
       </Flex>
     </Paper>
   );
